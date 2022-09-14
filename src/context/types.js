b6896000eb61a5e739b2fe6218545f4bb3e38e43
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-14 18:41:14 -0400

    types created

---
+++ src/context/types.js
@@ -0,0 +1,20 @@
+// darkMode
+export const SET_DARKMODE = 'SET_DARKMODE';
+export const SWITCH_DARKMODE = 'SWITCH_DARKMODE';
+
+// snackbar message
+export const SHOW_SNACKBAR_MESSAGE = 'SHOW_SNACKBAR_MESSAGE';
+export const CLOSE_SNACKBAR = 'CLOSE_SNACKBAR';
+
+// user management
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+// files list
+export const SET_FILES = 'SET_FILES';
+export const UPDATE_FILES = 'UPDATE_FILES';
+export const UPLOAD_FILE = 'UPLOAD_FILE';
+export const DELETE_FILE = 'DELETE_FILE';
+
+// all files list
+export const SET_ALL_FILES = 'SET_ALL_FILES';
