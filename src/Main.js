2208493f847e889773d7b7275709cf647010c152
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-15 17:00:47 -0400

    useEffect deleted

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import LoginScreen from './screens/Login';
