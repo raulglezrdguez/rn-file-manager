a502f03c44bdf6f4c032f425be7c3635426d1bea
raulglezrdguez <raulglezrdguez69@gmail.com>
2022-09-15 04:55:18 -0400

    Header created

---
+++ src/components/Header.js
@@ -0,0 +1,75 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useContext, useState} from 'react';
+import {Image} from 'react-native';
+import {
+  Appbar,
+  Avatar,
+  IconButton,
+  TouchableRipple,
+  useTheme,
+} from 'react-native-paper';
+import {useRoute} from '@react-navigation/native';
+
+import AppContext from '../context/AppContext';
+
+import {stringToColor} from '../util/utils';
+import config from '../../config';
+import FileManagerLogo from './FileManagerLogo';
+
+const Header = ({navigation, back}) => {
+  const theme = useTheme();
+  const route = useRoute();
+  const {darkMode, switchDarkMode, user, logout} = useContext(AppContext);
+
+  const [errors, setErrors] = useState({});
+
+  return (
+    <Appbar.Header>
+      {back ? (
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+      ) : (
+        <FileManagerLogo
+          height={50}
+          width={50}
+          stroke={theme.colors.text}
+          strokeWidth=".25"
+        />
+      )}
+      <Appbar.Content
+        title="FileManager"
+        subtitle="File manager service"
+        onPress={() => navigation.navigate('Home')}
+      />
+      {route.name === 'Home' && (
+        <>
+          <TouchableRipple onPress={() => navigation.navigate('Files')}>
+            <Avatar.Text
+              label={user?.name[0].toUpperCase()}
+              size={32}
+              style={{
+                backgroundColor: stringToColor(user?.nick || ''),
+                marginHorizontal: 10,
+              }}
+              labelStyle={{
+                textAlignVertical: 'center',
+                textAlign: 'center',
+              }}
+            />
+          </TouchableRipple>
+          <IconButton
+            icon={darkMode ? 'weather-sunny' : 'weather-night'}
+            size={32}
+            onPress={logout}
+          />
+        </>
+      )}
+      <IconButton
+        icon={darkMode ? 'weather-sunny' : 'weather-night'}
+        size={32}
+        onPress={switchDarkMode}
+      />
+    </Appbar.Header>
+  );
+};
+
+export default Header;
