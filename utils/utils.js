49a7136136efee1192d17c4581909361b77131cc
raulglezrdguez <raulglezrdguez69@gmail.com>
2022-09-15 04:37:44 -0400

    utils created

---
+++ utils/utils.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-bitwise */
+const stringToColor = string => {
+  let hash = 0;
+  let i;
+
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.substr(-2);
+  }
+
+  return color;
+};
+
+export const stringAvatar = (name, width = 32, height = 32) => {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+      width,
+      height,
+    },
+    children: `${name[0]}`,
+  };
+};
